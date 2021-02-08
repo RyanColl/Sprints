88af40ee07f5415b97e662dafe9f8cf9552c293f
Ryan <ryancollicutt22@gmail.com>
2021-02-08 14:21:37 -0800

    sprint complete, modal complete

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const PORT = 3000;
 const app = express()
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs'); 
 
@@ -11,6 +13,9 @@ app.set('view engine', 'ejs');
 app.get("/", (req, res) => { 
    res.render("pages/index", {})
 })
+app.post('/formData', function (req, res) {
+    res.render('pages/somefile', { name: req.body.name, email: req.body.email, message: req.body.message });
+});
 app.post("/myform", (req, res) => { // GET HTML form data from browser to server
     let formData = req.body;
     console.log(formData)
